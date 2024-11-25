c4299b1419403d548fb5cff2fbca8a0ea131e16e
lipedeoliveira <felipe.vieira.7721@gmail.com>
2024-11-25 10:59:18 -0300

    Modificação e tentando arrumar o login.kt e o cadastro de livros

--- app/src/main/java/com/example/ibm_app/MainActivity.kt
+++ app/src/main/java/com/example/ibm_app/MainActivity.kt
@@ -1,13 +1,43 @@
 // Caminho do arquivo: MainActivity.kt
 package com.example.ibm_app
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.ibm_app.ui.index
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.ibm_app.screens.LoginScreen
+import com.example.ibm_app.viewModel.LivroViewModel
 
 class MainActivity : ComponentActivity() {
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        index(this) // Chama o index
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val isLoginSuccessful = result.data?.getBooleanExtra("LOGIN_SUCCESSFUL", false) ?: false
+                if (isLoginSuccessful) {
+                    // Chame IBM_APP
+                    setContent {
+                        val navController = rememberNavController()
+                        val viewModel: LivroViewModel = viewModel()
+                        IBM_APP(navController, viewModel)
+                    }
+                }
+            }
+        }
+
+        setContent {
+            LoginScreen(launcher) { isLoginSuccessful ->
+                // ... (código para lidar com o resultado do login)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ibm_app/screens/login.kt
+++ app/src/main/java/com/example/ibm_app/screens/login.kt
@@ -1,6 +1,11 @@
 package com.example.ibm_app.screens
 
 import android.content.Intent
+import androidx.activity.addCallback
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,7 +13,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -22,14 +32,50 @@ import androidx.compose.ui.unit.sp
 import com.example.ibm_app.MainActivity
 import com.example.ibm_app.R // Certifique-se de que o ícone do Google esteja nesta pasta `res/drawable`
 import androidx.compose.ui.platform.LocalContext
+import androidx.room.Index
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.ibm_app.roomDB.Administrador
+import com.example.ibm_app.roomDB.AdministradorDataBase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen() {
+fun LoginScreen(launcher: ActivityResultLauncher<Intent>) {
     // Contexto para navegação entre atividades
+    var emailInserido by remember { mutableStateOf("") }
+    var senhaInserida by remember { mutableStateOf("") }
     val context = LocalContext.current
+    val db = Room.databaseBuilder(
+        context.applicationContext,
+        AdministradorDataBase::class.java,
+        "administrador_database"
+    )
+
+
+        .addCallback(object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                // Insira os dados do administrador aqui
+                val administrador = Administrador(nome = "Admin", email = "admin@example.com", senha = "adm123") // Substitua pelos dados desejados
+                val administradorDao = AdministradorDataBase.getInstance(context).administradorDao()
+                // Insira o administrador em uma coroutine, pois é uma operação de suspensão
+                GlobalScope.launch(Dispatchers.IO) {
+                    administradorDao.inserirAdministrador(administrador)
+                }
+            }
+        })
+        .build()
+    val administradorDao = db.administradorDao()
     val scope = rememberCoroutineScope()
+    var showSnackbar by remember { mutableStateOf(false) }
+    val snackbarHostState = remember { SnackbarHostState() }
+
+
 
     Box(
         modifier = Modifier
@@ -80,9 +126,11 @@ fun LoginScreen() {
                 }
             }
             Spacer(modifier = Modifier.height(32.dp))
+            var emailInserido by remember { mutableStateOf("") }
+            var senhaInserida by remember { mutableStateOf("") }
             TextField(
-                value = "",
-                onValueChange = { /* Lógica para atualizar o texto */ },
+                value = emailInserido,
+                onValueChange = {emailInserido = it},
                 label = { Text("Email") },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(8.dp),
@@ -94,8 +142,8 @@ fun LoginScreen() {
             )
             Spacer(modifier = Modifier.height(24.dp)) // Maior espaço entre os campos
             TextField(
-                value = "",
-                onValueChange = { /* Lógica para atualizar o texto */ },
+                value = senhaInserida,
+                onValueChange = { senhaInserida = it },
                 label = { Text("Senha") },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(8.dp),
@@ -117,22 +165,7 @@ fun LoginScreen() {
                 textAlign = TextAlign.Start
             )
             Spacer(modifier = Modifier.height(24.dp))
-            Button(
-                onClick = {
-                    // Lógica para ir à MainActivity
-                    scope.launch {
-                        val intent = Intent(context, MainActivity::class.java)
-                        context.startActivity(intent)
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp),
-                shape = RoundedCornerShape(8.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA8C5A8)) // Verde claro
-            ) {
-                Text(text = "Entrar", fontSize = 16.sp, color = Color.White)
-            }
+
             Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = "Criar nova conta",
@@ -141,12 +174,44 @@ fun LoginScreen() {
                 modifier = Modifier.clickable { /* Lógica para criar conta */ },
                 fontWeight = FontWeight.Bold
             )
+
+        }
+            Button(onClick = {
+                scope.launch {
+                    val administrador = administradorDao.getAdministradorPorEmail(emailInserido).firstOrNull()
+                    if (administrador != null && administrador.senha == senhaInserida) {
+                        // Redirecione para home.kt
+                        val intent = Intent().putExtra("LOGIN_SUCCESSFUL", true)
+                        launcher.launch(intent)
+                    } else {
+                        // Atualize o estado para exibir o Snackbar
+                        showSnackbar = true
+                    }
+                }
+            }) {
+                // ...
+            }
+
+            // Observe o estado e exiba o Snackbar quando necessário
+            LaunchedEffect(showSnackbar) {
+                if (showSnackbar) {
+                    snackbarHostState.showSnackbar(
+                        message = "Email ou senha incorretos",
+                        duration = SnackbarDuration.Short
+                    )
+                    // Redefina o estado após exibir o Snackbar
+                    showSnackbar = false
+                }
+            }
         }
     }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun LoginScreenPreview() {
-    LoginScreen()
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {}
+    LoginScreen(launcher)
+
+
 }
